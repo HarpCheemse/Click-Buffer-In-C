26b5a0bffde726670a5c16c4b8e9757736ca6f41
HarpCheemse <sloshyteacher@gmail.com>
2025-04-09 17:41:02 +0700

    added input validation and rate to 10

--- ClickBuffer.c
+++ ClickBuffer.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include <pthread.h>
+#include <stdbool.h>
 
 #define PRINT_RED "\x1b[31m" 
 #define PRINT_GREEN "\x1b[32m"
@@ -9,16 +10,78 @@
 #define PRINT_GOLD "\033[1;33m"
 #define PRINT_LIGHT_AQUA "\033[1;33m""\x1b[36m"
 #define PRINT_COLOR_RESET "\x1b[0m" 
+#define ERROR -1
+#define MAX_SIZE 255
 
 int TOGGLE_CLICK_BUFFER = 1;
 int CLICK_BUFFER_RATE = 1;
 int CLICK_PER_SECOND = 0;
+int sleep_for = 1;
 
 void getKeybind();
 void printMenu();
 void changeCPS();
 void refreshMenu();
+void printUserPrompt();
 
+/*                     Input Validation              */
+void clearBuffer()
+{
+    int ch;
+    while(ch = getchar() != '\n') {}
+}
+bool intInputHandler(const char* input)
+{
+	int index = 0;
+	if(input[0] =='.') return false;
+	if(input[0] =='-') return false;
+    int length = strlen(input);
+    for(; index<length; index++)
+    {
+        if(input[index] <='9' && input[index]>='0') continue;
+        else return false;
+    }
+    return true;
+}
+int intInput() 
+{
+	char buffer[MAX_SIZE];
+    while(1)
+    {
+        fgets(buffer, MAX_SIZE, stdin);
+        buffer[strcspn(buffer,"\n")] = '\0';
+        if(intInputHandler(buffer)) return atoi(buffer);
+        else return ERROR; 
+    }
+}
+/*                     Input Function                */
+void change_CLICK_BUFFER_RATE()
+{
+    clearBuffer();
+    printf(PRINT_LIGHT_AQUA"\nChange Click Buffer Rate: "PRINT_COLOR_RESET);
+    while(1)
+    {
+        int temp = intInput();
+        if(temp == ERROR)
+        {
+            refreshMenu();
+            printf(PRINT_RED"Invalid Input!\n"PRINT_COLOR_RESET);
+            printf(PRINT_LIGHT_AQUA"Change Click Buffer Rate: "PRINT_COLOR_RESET);
+        }
+        else if(temp <= 0 || temp > 10)
+        {
+            refreshMenu();
+            printf(PRINT_RED"Please Enter Integer Number Between 1 And 10"PRINT_COLOR_RESET);
+            printf(PRINT_LIGHT_AQUA"\nChange Click Buffer Rate: "PRINT_COLOR_RESET);
+        }
+        else
+        {
+            CLICK_BUFFER_RATE = temp;
+            return;
+        }
+    }
+}
+/*                     MENU FUNCTION                 */
 void printAboutProgram()
 {
     refreshMenu();
@@ -29,19 +92,9 @@ void printAboutProgram()
     printf("\nContact: [harpcheemse@gmail.com]" PRINT_COLOR_RESET);
     printf(PRINT_AQUA"\n===========================================================================\n"PRINT_COLOR_RESET);
 }
-void clearBuffer()
-{
-    int ch;
-    while(ch = getchar() != '\n') {}
-}
-void refreshMenu()
-{
-    system("cls");
-    printMenu();
-}
 void printMenu()
 {
-    char line[50] = "=======================================";
+    char line[255] = "============================================================";
     printf(PRINT_AQUA"%s\n"PRINT_COLOR_RESET, line);
     printf(PRINT_GOLD"          Click Buffer        \n");
     printf("1. Toggle Click Buffer. Currently %s%s%s\n",
@@ -54,18 +107,14 @@ void printMenu()
     printf("4. Exit The Program\n"PRINT_COLOR_RESET);
     printf(PRINT_AQUA"%s\n"PRINT_COLOR_RESET, line);
 }
-int getUserCPS()
+void refreshMenu()
 {
-
+    system("cls");
+    printMenu();
 }
 void printUserPrompt()
 {
     printf(PRINT_LIGHT_AQUA"\nEnter Your Choice: "PRINT_COLOR_RESET);
-}
-void leftClick()
-{
-    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 }
 void menuHandler()
 {
@@ -77,17 +126,16 @@ void menuHandler()
         scanf("%d", &user_selection);
         switch (user_selection)
         {
-        case 1:  // change CPS
+        case 1:  // Toggle Click Buffer ON/OFF
             refreshMenu();
             printUserPrompt();
             TOGGLE_CLICK_BUFFER = !TOGGLE_CLICK_BUFFER;
             refreshMenu();
             printUserPrompt();
             break;
-        case 2: // KeyBind
+        case 2: // Change Click Buffer Rate
             refreshMenu();
-            printUserPrompt();
-            scanf("%d", &CLICK_BUFFER_RATE);
+            change_CLICK_BUFFER_RATE();
             refreshMenu();
             printUserPrompt();
             break;
@@ -112,39 +160,74 @@ void menuHandler()
         }
     }
 }
-void changeCPS()
+/*                    Functionality                  */
+void leftClick()
 {
-    refreshMenu();
-    printf(PRINT_LIGHT_AQUA"\nChange CPS: "PRINT_COLOR_RESET);
-    scanf("%d", &CLICK_PER_SECOND);
-    return;
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 }
 void* clickEvent(void* arg)
 {
-    const DWORD HOLD_THRESHOLD = 200;
-    DWORD current_click_time;
-    DWORD last_click_time;
+    const DWORD HOLD_THRESHOLD = 100;
+    const DWORD WAIT_THRESHOLD = 200;
+    DWORD first_last_click_time;
+    DWORD second_last_click_time;
     while(1)
     {
         int is_holding_key = 0;
+        int out_of_clicking_window_time = 0;
         if ((GetAsyncKeyState(VK_LBUTTON) & 0x8000) && TOGGLE_CLICK_BUFFER)
         {
-            last_click_time = GetTickCount();
+            first_last_click_time = GetTickCount();
             while(GetAsyncKeyState(VK_LBUTTON) & 0x8000)
             {
-                current_click_time = GetTickCount();
-                if(current_click_time-last_click_time>HOLD_THRESHOLD)
+                DWORD first_current_click_time = GetTickCount();
+                if( first_current_click_time - first_last_click_time>HOLD_THRESHOLD)
                 {
-                    is_holding_key = 1;
+                    is_holding_key = 1;  // no break to consume leftover
                 }
                 Sleep(1);
             }
-            if(!is_holding_key)
+            if(!is_holding_key) //detect the first click
             {
-                for(int i = 1; i<= CLICK_BUFFER_RATE; i++)
+                START_LOOP:
+                DWORD start_timer = GetTickCount();
+                while(1)
                 {
-                    leftClick();
-                    Sleep(10);
+                    DWORD current_timer = GetTickCount();
+                    if(current_timer - start_timer >= WAIT_THRESHOLD)  //detect if the second click inside the WAIT_threshold to start click buffer
+                    {
+                        out_of_clicking_window_time = 1;
+                        break;
+                    }
+                    if((GetAsyncKeyState(VK_LBUTTON) & 0x8000) && TOGGLE_CLICK_BUFFER)
+                    {
+                        second_last_click_time = GetTickCount();
+                        while((GetAsyncKeyState(VK_LBUTTON) & 0x8000) && TOGGLE_CLICK_BUFFER)  //check if the second click is a click or a hold
+                        {
+                            DWORD second_current_click_time = GetTickCount();
+                            if(second_current_click_time - second_last_click_time > HOLD_THRESHOLD)
+                            {
+                                is_holding_key = 1; // no break to consume leftover
+                            }
+                            Sleep(1);
+                        }
+                        break;
+                    } 
+                    
+                }
+                if(!is_holding_key && !out_of_clicking_window_time)
+                {
+                    for(int i = 0; i< CLICK_BUFFER_RATE;i++)
+                    {
+                        int first_random_sleep = (rand() % (20)) + 2;
+                        int second_random_sleep = (rand() % (20)) + 2;
+                        Sleep((second_last_click_time-first_last_click_time)/first_random_sleep);
+                        leftClick();
+                        Sleep((second_last_click_time-first_last_click_time)/second_random_sleep);
+                        leftClick();
+                        goto START_LOOP;
+                    }
                 }
             }
         }
